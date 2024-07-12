9f766766284787cc21be08b0978fa5d075c6753c
Mohit Bhatt <mohitbha@dhcp-10-120-137-34.wifi.oracle.com>
2024-07-12 14:32:33 +0530

    this is chnge

--- src/main/java/com/training/bms/MessagingApiAppApplication.java
+++ src/main/java/com/training/bms/MessagingApiAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MessagingApiAppApplication {
 	
 	public String printGreetings() {
-		return "Hi Oracle" ;
+		return "Hiiii Oracle" ;
 	}
 
 	public static void main(String[] args) {
